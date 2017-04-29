71eac0762d35c2aa0339ced1c8ffbbfdbe3eceff
Bjarni Kristinn <Bjarnikristinn@simnet.is>
2017-04-29 13:25:46 -0700

    Ice

---
+++ ICESolution/ICE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ICE.Startup))]
+namespace ICE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
